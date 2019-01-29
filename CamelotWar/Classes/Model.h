9c8501337bcf904ab380ad5b4fe9352f94660adc
Long Dao <daongocchanhlong@gmail.com>
2019-01-29 09:53:51 +0700

    +

--- CamelotWar/Classes/Model.h
+++ CamelotWar/Classes/Model.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "cocos2d.h"
+#include <string>
 class Model
 {
 public:
@@ -11,6 +12,10 @@ public:
 
 	cocos2d::Vec2 getPos();
 	void setPos(cocos2d::Vec2 pos);
+
+	void setAnimation(std::string namePlist, char * namePng, int CountImage, cocos2d::Vec2 pos, cocos2d::Scene * scene);
+
+	
 
 	virtual void update() = 0;
 	virtual void init() = 0;
