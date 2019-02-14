98fbae46cf9edf38fc2d8777b791317b80ee7f51
Long Dao <daongocchanhlong@gmail.com>
2019-02-14 09:22:22 +0700

    + add all

--- CamelotWar/Classes/Character.h
+++ CamelotWar/Classes/Character.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "cocos2d.h"
 #include "Model.h"
-class Character : Model
+class Character : public Model
 {
 public:
 	Character(); 
--- CamelotWar/Classes/Troll.cpp
+++ CamelotWar/Classes/Troll.cpp
@@ -1,9 +1,24 @@
 #include "Troll.h"
+#include "Defines.h"
 
 Troll::Troll()
 {
 }
 
 Troll::~Troll()
+{
+}
+
+Troll::Troll(cocos2d::Scene * scene)
+{
+	setAnimation(NAME_PLIST_TROLL, NAME_PNG_TROLL,COUNT_IMG_TROLL,cocos2d::Vec2(250,200), scene);
+	
+}
+
+void Troll::update()
+{
+}
+
+void Troll::init()
 {
 }
